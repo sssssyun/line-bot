5f4f27d22420edddebc8e8dd6cf9855e5b5c573e
sssssyun <wsyun0529@gmail.com>
2022-05-30 17:12:19 +0800

    fix5

--- app.py
+++ app.py
@@ -39,15 +39,14 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     r = "很抱歉你在問什麼？"
-
     if msg == 'hi':
-        r == 'hi'
+        r = 'hi'
     elif msg == '吃飽沒'
-        r == '還沒'
+        r = '還沒'
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        TextSendMessage(text= r))
 
 
 if __name__ == "__main__":
