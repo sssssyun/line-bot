519d500cf181a05113e5b9c8fda77349c4dd432c
sssssyun <wsyun0529@gmail.com>
2022-05-30 16:56:40 +0800

    fix4

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='你吃飽了嗎?'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
